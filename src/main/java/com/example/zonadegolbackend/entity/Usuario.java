9f7ef2dae6ebd18b70ce9de386776808dac68def
davidmato <davidmato2005@gmail.com>
2025-03-12 20:28:46 +0100

    entity de usuario

---
+++ src/main/java/com/example/zonadegolbackend/entity/Usuario.java
@@ -0,0 +1,67 @@
+package com.example.zonadegolbackend.entity;
+
+
+import com.example.zonadegolbackend.enums.Rol;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "usuario", schema = "zona_de_gol", catalog = "postgres")
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+
+    @Column(name = "username", nullable = false)
+    private String username;
+
+
+    @Column(name = "correo", nullable = false)
+    private String correo;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "rol", nullable = false)
+    @Enumerated(EnumType.ORDINAL)
+    private Rol rol;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
