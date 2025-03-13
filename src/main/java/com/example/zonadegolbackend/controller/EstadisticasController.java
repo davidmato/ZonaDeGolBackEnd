e5a46795b380ad0e79f7895ad5135f96072c01d8
MarioCastroRamirez <mcrcdr@gmail.com>
2025-03-13 19:51:31 +0100

    Añadidos metodos de crear editar y eliminar estadisticas y sus controladores

--- src/main/java/com/example/zonadegolbackend/controller/EstadisticasController.java
+++ src/main/java/com/example/zonadegolbackend/controller/EstadisticasController.java
@@ -1,11 +1,29 @@
 package com.example.zonadegolbackend.controller;
 
+import com.example.zonadegolbackend.entity.Estadisticas;
+import com.example.zonadegolbackend.services.EstadisticasService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/estadisticas")
 public class EstadisticasController {
+
+    private final EstadisticasService estadisticasService;
+
+    @PostMapping("/crear")
+    public Estadisticas crearEstadisticas(@RequestBody Estadisticas estadisticas) {
+        return estadisticasService.crearEstadisticas(estadisticas);
+    }
+
+    @PutMapping("/editar/{id}")
+    public Estadisticas editarEstadisticas(@PathVariable Integer id, @RequestBody Estadisticas estadisticas) {
+        return estadisticasService.editarEstadisticas(id, estadisticas);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminarEstadisticas(@PathVariable Integer id) {
+        estadisticasService.eliminarEstadisticas(id);
+    }
 }
--- src/main/java/com/example/zonadegolbackend/services/EstadisticasService.java
+++ src/main/java/com/example/zonadegolbackend/services/EstadisticasService.java
@@ -1,9 +1,55 @@
 package com.example.zonadegolbackend.services;
 
+import com.example.zonadegolbackend.dtos.CrearEquipo;
+import com.example.zonadegolbackend.entity.Entrenador;
+import com.example.zonadegolbackend.entity.Equipo;
+import com.example.zonadegolbackend.entity.Estadisticas;
+import com.example.zonadegolbackend.entity.Jugador;
+import com.example.zonadegolbackend.repository.EstadisticasRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
 public class EstadisticasService {
+
+    private final EstadisticasRepository estadisticasRepository;
+
+    public Estadisticas crearEstadisticas(Estadisticas estadisticas) {
+
+        Estadisticas nuevaEstadisticas = new Estadisticas();
+
+        nuevaEstadisticas.setPartidosJugados(estadisticas.getPartidosJugados());
+        nuevaEstadisticas.setGoles(estadisticas.getGoles());
+        nuevaEstadisticas.setAsistencias(estadisticas.getAsistencias());
+        nuevaEstadisticas.setTarjetasAmarillas(estadisticas.getTarjetasAmarillas());
+        nuevaEstadisticas.setTarjetasRojas(estadisticas.getTarjetasRojas());
+        nuevaEstadisticas.setPorteriaCero(estadisticas.getPorteriaCero());
+        nuevaEstadisticas.setTemporada(estadisticas.getTemporada());
+        nuevaEstadisticas.setJugador(estadisticas.getJugador());
+
+        return estadisticasRepository.save(estadisticas);
+    }
+
+    public Estadisticas editarEstadisticas(Integer idEstadisticas, Estadisticas estadisticas) {
+        Estadisticas estadisticasExistente = estadisticasRepository.findById(idEstadisticas)
+                .orElseThrow(() -> new RuntimeException("Estadisticas no encontradas"));
+
+        estadisticasExistente.setPartidosJugados(estadisticas.getPartidosJugados());
+        estadisticasExistente.setGoles(estadisticas.getGoles());
+        estadisticasExistente.setAsistencias(estadisticas.getAsistencias());
+        estadisticasExistente.setTarjetasAmarillas(estadisticas.getTarjetasAmarillas());
+        estadisticasExistente.setTarjetasRojas(estadisticas.getTarjetasRojas());
+        estadisticasExistente.setPorteriaCero(estadisticas.getPorteriaCero());
+        estadisticasExistente.setTemporada(estadisticas.getTemporada());
+        estadisticasExistente.setJugador(estadisticas.getJugador());
+
+        return estadisticasRepository.save(estadisticasExistente);
+    }
+
+    public void eliminarEstadisticas(Integer id) {
+        estadisticasRepository.deleteById(id);
+    }
 }
