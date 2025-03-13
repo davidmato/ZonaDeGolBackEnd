7cf3ebc51ef65939e8e6dd803492d9eaeafb6f5d
davidmato <davidmato2005@gmail.com>
2025-03-13 03:13:16 +0100

    metodo para crear equipo en el service

--- src/main/java/com/example/zonadegolbackend/services/EquipoService.java
+++ src/main/java/com/example/zonadegolbackend/services/EquipoService.java
@@ -1,7 +1,13 @@
 package com.example.zonadegolbackend.services;
 
+import com.example.zonadegolbackend.dtos.CrearEquipo;
+import com.example.zonadegolbackend.entity.Entrenador;
 import com.example.zonadegolbackend.entity.Equipo;
+import com.example.zonadegolbackend.entity.Usuario;
+import com.example.zonadegolbackend.enums.Rol;
+import com.example.zonadegolbackend.repository.EntrenadorRepository;
 import com.example.zonadegolbackend.repository.EquipoRepository;
+import com.example.zonadegolbackend.repository.UsuarioRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +18,39 @@ import java.util.List;
 public class EquipoService {
 
     private final EquipoRepository equipoRepository;
+    private final UsuarioRepository usuarioRepository;
+    private final EntrenadorRepository entrenadorRepository;
 
     public List<Equipo> findAll() {
         return equipoRepository.findAll();
     }
 
+
+
+    public Equipo create(CrearEquipo crearEquipo) {
+        Usuario usuario = new Usuario();
+        usuario.setUsername(crearEquipo.getUsername());
+        usuario.setCorreo(crearEquipo.getCorreo());
+        usuario.setPassword(crearEquipo.getPassword());
+        usuario.setRol(Rol.ENTRENADOR);
+        usuarioRepository.save(usuario);
+
+        Entrenador entrenador = new Entrenador();
+        entrenador.setNombre(crearEquipo.getNombreEntrenador());
+        entrenador.setApellido(crearEquipo.getApellido());
+        entrenador.setFechaNacimiento(crearEquipo.getFechaNacimiento());
+        entrenador.setImagen(crearEquipo.getImagenEntrenador());
+        entrenador.setDni(crearEquipo.getDni());
+        entrenador.setUsuario(usuario);
+        entrenadorRepository.save(entrenador);
+
+        Equipo equipo = new Equipo();
+        equipo.setNombre(crearEquipo.getNombre());
+        equipo.setDescripcion(crearEquipo.getDescripcion());
+        equipo.setFechaFundacion(crearEquipo.getFechaFundacion());
+        equipo.setImagen(crearEquipo.getImagen());
+        equipo.setEntrenador(entrenador);
+
+        return equipoRepository.save(equipo);
+    }
 }
