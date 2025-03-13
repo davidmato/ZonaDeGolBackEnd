251e6450da3a01f8b0d00fa35a60851fed894164
davidmato <davidmato2005@gmail.com>
2025-03-13 01:22:36 +0100

    clasificacion controller

---
+++ src/main/java/com/example/zonadegolbackend/controller/ClasificacionController.java
@@ -0,0 +1,9 @@
+package com.example.zonadegolbackend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ClasificacionController {
+}
