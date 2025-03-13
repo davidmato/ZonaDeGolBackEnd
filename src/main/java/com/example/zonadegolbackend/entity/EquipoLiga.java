ee518c3c255338da457e1bd312715ed0f91f6ee0
davidmato <davidmato2005@gmail.com>
2025-03-13 01:20:09 +0100

    equipoLiga entity

---
+++ src/main/java/com/example/zonadegolbackend/entity/EquipoLiga.java
@@ -0,0 +1,31 @@
+package com.example.zonadegolbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "equipo_liga", schema = "zona_de_gol", catalog = "postgres")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class EquipoLiga {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_equipo", nullable = false)
+    private Equipo equipo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_liga", nullable = false)
+    private Liga liga;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_temporada", nullable = false)
+    private Temporada temporada;
+}
