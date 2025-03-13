10c6713cc00849929466527fd6ad90b4c471312b
davidmato <davidmato2005@gmail.com>
2025-03-13 16:27:29 +0100

    metodo de asociar un jugador a un equipo

--- src/main/java/com/example/zonadegolbackend/services/JugadorService.java
+++ src/main/java/com/example/zonadegolbackend/services/JugadorService.java
@@ -1,9 +1,14 @@
 package com.example.zonadegolbackend.services;
 
+import com.example.zonadegolbackend.entity.Equipo;
+import com.example.zonadegolbackend.entity.EquipoJugador;
 import com.example.zonadegolbackend.entity.Jugador;
+import com.example.zonadegolbackend.repository.EquipoJugadorRepository;
+import com.example.zonadegolbackend.repository.EquipoRepository;
 import com.example.zonadegolbackend.repository.JugadorRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,13 +16,21 @@ import java.util.List;
 @AllArgsConstructor
 public class JugadorService {
 
+    private final EquipoRepository equipoRepository;
     private final JugadorRepository jugadorRepository;
+    private final EquipoJugadorRepository equipoJugadorRepository;
+
 
     public List<Jugador> findAll() {
         return jugadorRepository.findAll();
     }
 
-    public Jugador crearJugador (Jugador jugador) {
+    public Jugador findById(Integer id) {
+        return jugadorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Jugador no encontrado"));
+    }
+
+    public Jugador crearJugador(Jugador jugador) {
 
         Jugador nuevoJugador = new Jugador();
 
@@ -31,5 +44,22 @@ public class JugadorService {
         nuevoJugador.setUsuario(jugador.getUsuario());
 
         return jugadorRepository.save(nuevoJugador);
+    }
+
+
+    @Transactional
+    public void associatePlayersWithTeam(Integer idEquipo, List<Integer> idJugadores) {
+        Equipo equipo = equipoRepository.findById(idEquipo)
+                .orElseThrow(() -> new RuntimeException("Equipo no encontrado"));
+
+        for (Integer idJugador : idJugadores) {
+            Jugador jugador = jugadorRepository.findById(idJugador)
+                    .orElseThrow(() -> new RuntimeException("Jugador no encontrado"));
+
+            EquipoJugador equipoJugador = new EquipoJugador();
+            equipoJugador.setEquipo(equipo);
+            equipoJugador.setJugador(jugador);
+            equipoJugadorRepository.save(equipoJugador);
+        }
     }
 }
