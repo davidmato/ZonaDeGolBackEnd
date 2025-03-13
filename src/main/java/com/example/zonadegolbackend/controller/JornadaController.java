75be0a32b8132a5436030d41a3e32e6b4b6629d0
MarioCastroRamirez <mcrcdr@gmail.com>
2025-03-13 20:47:23 +0100

    Creados metodos para crear, editar y eliminar jornadas y sus controladores

--- src/main/java/com/example/zonadegolbackend/controller/JornadaController.java
+++ src/main/java/com/example/zonadegolbackend/controller/JornadaController.java
@@ -1,5 +1,7 @@
 package com.example.zonadegolbackend.controller;
 
+import com.example.zonadegolbackend.entity.Jornada;
+import com.example.zonadegolbackend.services.JornadaService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/jornada")
 public class JornadaController {
+
+    private final JornadaService jornadaService;
+
+    @PostMapping("/crear")
+    public Jornada crearJornada(@RequestBody Jornada jornada) {
+        return jornadaService.crearJornada(jornada);
+    }
+
+    @PutMapping("/editar/{id}")
+    public Jornada editarJornada(@PathVariable Integer id, @RequestBody Jornada jornada) {
+        return jornadaService.editarJornada(id, jornada);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminarJornada(@PathVariable Integer id) {
+        jornadaService.eliminarJornada(id);
+    }
 }
--- src/main/java/com/example/zonadegolbackend/services/JornadaService.java
+++ src/main/java/com/example/zonadegolbackend/services/JornadaService.java
@@ -1,9 +1,41 @@
 package com.example.zonadegolbackend.services;
 
+import com.example.zonadegolbackend.entity.Jornada;
+import com.example.zonadegolbackend.repository.JornadaRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class JornadaService {
+
+    private final JornadaRepository jornadaRepository;
+
+    public Jornada crearJornada(Jornada jornada) {
+
+        Jornada nuevaJornada = new Jornada();
+
+        nuevaJornada.setFecha(jornada.getFecha());
+        nuevaJornada.setEquipoLocal(jornada.getEquipoLocal());
+        nuevaJornada.setEquipoVisitante(jornada.getEquipoVisitante());
+        nuevaJornada.setTemporada(jornada.getTemporada());
+
+        return jornadaRepository.save(jornada);
+    }
+
+    public Jornada editarJornada(Integer idJornada, Jornada jornada) {
+        Jornada jornadaExistente = jornadaRepository.findById(idJornada)
+                .orElseThrow(() -> new RuntimeException("Jornada no encontrada"));
+
+        jornadaExistente.setFecha(jornada.getFecha());
+        jornadaExistente.setEquipoLocal(jornada.getEquipoLocal());
+        jornadaExistente.setEquipoVisitante(jornada.getEquipoVisitante());
+        jornadaExistente.setTemporada(jornada.getTemporada());
+
+        return jornadaRepository.save(jornadaExistente);
+    }
+
+    public void eliminarJornada(Integer id) {
+        jornadaRepository.deleteById(id);
+    }
 }
