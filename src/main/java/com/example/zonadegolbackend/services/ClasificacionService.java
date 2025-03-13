a4ad7203f217eae887c7a625c0f6649e353b5f65
vlexx91 <alemartinenghi@gmail.com>
2025-03-13 03:13:18 +0100

    Metodo crear y findAll en Clasificacion

--- src/main/java/com/example/zonadegolbackend/services/ClasificacionService.java
+++ src/main/java/com/example/zonadegolbackend/services/ClasificacionService.java
@@ -1,9 +1,38 @@
 package com.example.zonadegolbackend.services;
 
+import com.example.zonadegolbackend.entity.Clasificacion;
+import com.example.zonadegolbackend.repository.ClasificacionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class ClasificacionService {
+
+    private final ClasificacionRepository clasificacionRepository;
+
+    public List<Clasificacion> findAll() {
+        return clasificacionRepository.findAll();
+    }
+
+    //AÚN POR PROBAR
+    public Clasificacion crearClasificacion (Clasificacion clasificacion) {
+
+        Clasificacion nuevoClasificacion = new Clasificacion();
+
+        nuevoClasificacion.setPuesto(clasificacion.getPuesto());
+        nuevoClasificacion.setVictorias(clasificacion.getVictorias());
+        nuevoClasificacion.setEmpates(clasificacion.getEmpates());
+        nuevoClasificacion.setDerrotas(clasificacion.getDerrotas());
+        nuevoClasificacion.setGolAFavor(clasificacion.getGolAFavor());
+        nuevoClasificacion.setGolEnContra(clasificacion.getGolEnContra());
+        nuevoClasificacion.setGolDiferencia(clasificacion.getGolDiferencia());
+        nuevoClasificacion.setPuntos(clasificacion.getPuntos());
+        nuevoClasificacion.setEquipoLiga(clasificacion.getEquipoLiga());
+
+        return clasificacionRepository.save(nuevoClasificacion);
+
+    }
 }
