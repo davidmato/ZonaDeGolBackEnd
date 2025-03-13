17f7874d005da0981d6c3e2825b086a0188ed2df
davidmato <davidmato2005@gmail.com>
2025-03-13 03:12:33 +0100

    crear entrenador en controlador

--- src/main/java/com/example/zonadegolbackend/controller/EntrenadorController.java
+++ src/main/java/com/example/zonadegolbackend/controller/EntrenadorController.java
@@ -1,6 +1,11 @@
 package com.example.zonadegolbackend.controller;
 
+import com.example.zonadegolbackend.dtos.CrearEntrenador;
+import com.example.zonadegolbackend.entity.Entrenador;
+import com.example.zonadegolbackend.services.EntrenadorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/entrenador")
 public class EntrenadorController {
+
+
+    private final EntrenadorService entrenadorService;
+
+    @PostMapping("/crear")
+    public Entrenador crearEntrenador(@RequestBody CrearEntrenador crearEntrenador) {
+        return entrenadorService.create(crearEntrenador);
+    }
 }
