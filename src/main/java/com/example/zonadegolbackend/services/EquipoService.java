85767aec34b094cd09b03c1dd90759fe4e6a38e1
davidmato <davidmato2005@gmail.com>
2025-03-13 01:38:05 +0100

    request mapping hechos en los controladores

--- src/main/java/com/example/zonadegolbackend/services/EquipoService.java
+++ src/main/java/com/example/zonadegolbackend/services/EquipoService.java
@@ -1,9 +1,20 @@
 package com.example.zonadegolbackend.services;
 
+import com.example.zonadegolbackend.entity.Equipo;
+import com.example.zonadegolbackend.repository.EquipoRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AllArgsConstructor
 public class EquipoService {
+
+    private final EquipoRepository equipoRepository;
+
+    public List<Equipo> findAll() {
+        return equipoRepository.findAll();
+    }
+
 }
