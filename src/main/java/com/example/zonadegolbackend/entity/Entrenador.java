a81a3221e04212465ef336fb66115195f61f9f73
davidmato <davidmato2005@gmail.com>
2025-03-13 01:19:43 +0100

    entrenador entity

---
+++ src/main/java/com/example/zonadegolbackend/entity/Entrenador.java
@@ -0,0 +1,45 @@
+package com.example.zonadegolbackend.entity;
+
+import com.example.zonadegolbackend.enums.Posicion;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "entrenador", schema = "zona_de_gol", catalog = "postgres")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Entrenador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "apellido", nullable = false)
+    private String apellido;
+
+    @Column(name = "fecha_nacimiento", nullable = false)
+    @DateTimeFormat()
+    private LocalDateTime fechaNacimiento;
+
+
+    @Column(name = "dni", nullable = false)
+    private String dni;
+
+    @Column(name = "imagen", nullable = true)
+    private String imagen;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_usuario")
+    private Usuario usuario;
+}
