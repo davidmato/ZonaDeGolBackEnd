effc2d5ff64bd97b7ab0342f380b0090d50089eb
davidmato <davidmato2005@gmail.com>
2025-03-13 17:48:07 +0100

    metodo de resgitro y login llamado en el controlador

--- src/main/java/com/example/zonadegolbackend/controller/UsuarioController.java
+++ src/main/java/com/example/zonadegolbackend/controller/UsuarioController.java
@@ -1,8 +1,12 @@
 package com.example.zonadegolbackend.controller;
 
+import com.example.zonadegolbackend.dtos.AuthenticationDTO;
+import com.example.zonadegolbackend.dtos.UsuarioDto;
 import com.example.zonadegolbackend.entity.Usuario;
 import com.example.zonadegolbackend.services.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +15,22 @@ import org.springframework.web.bind.annotation.*;
 public class UsuarioController {
 
     private final UsuarioService usuarioService;
+
+
+    @PostMapping("/register")
+    public AuthenticationDTO register(@RequestBody UsuarioDto userDTO) {
+        return usuarioService.register(userDTO);
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
+    @PostMapping("/login")
+    public AuthenticationDTO login(@RequestBody UsuarioDto usuarioDTO) {
+        logger.debug("Login method called with usuarioDTO: {}", usuarioDTO);
+        return usuarioService.login(usuarioDTO);
+    }
+
+
 
     @PostMapping("/crear")
     public void crearUsuario(@RequestBody Usuario usuario) {
