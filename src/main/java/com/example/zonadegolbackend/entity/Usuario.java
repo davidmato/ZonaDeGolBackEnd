467a9d50a8eae398112791352be92b83c8a87d90
vlexx91 <alemartinenghi@gmail.com>
2025-03-14 15:20:44 +0100

    bug 415

--- src/main/java/com/example/zonadegolbackend/entity/Usuario.java
+++ src/main/java/com/example/zonadegolbackend/entity/Usuario.java
@@ -2,6 +2,7 @@ package com.example.zonadegolbackend.entity;
 
 
 import com.example.zonadegolbackend.enums.Rol;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
