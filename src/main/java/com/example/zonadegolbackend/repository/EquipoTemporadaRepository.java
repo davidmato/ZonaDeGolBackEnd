1f3a755d87485c8e09b4855deeaedde93d2904ed
davidmato <davidmato2005@gmail.com>
2025-03-26 21:30:45 +0100

    eliminacion de codigo no utilizado

--- src/main/java/com/example/zonadegolbackend/repository/EquipoTemporadaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.zonadegolbackend.repository;
-
-import com.example.zonadegolbackend.entity.EquipoTemporada;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EquipoTemporadaRepository extends JpaRepository<EquipoTemporada, Integer> {
-}
