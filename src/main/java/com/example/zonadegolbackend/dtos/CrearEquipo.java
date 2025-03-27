1e4baa073f5d962f12bf34202d28cdaa359f8210
davidmato <davidmato2005@gmail.com>
2025-03-27 10:51:45 +0100

    cambio localDateTime por LocalDate

--- src/main/java/com/example/zonadegolbackend/dtos/CrearEquipo.java
+++ src/main/java/com/example/zonadegolbackend/dtos/CrearEquipo.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 public class CrearEquipo {
     private String nombre;
     private String descripcion;
-    private LocalDateTime fechaFundacion;
+    private LocalDate fechaFundacion;
     private String imagen;
     private String nombreEntrenador;
     private String apellido;
