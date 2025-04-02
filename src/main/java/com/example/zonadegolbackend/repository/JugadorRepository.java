617391fad72c2ac872e57445eab8a1469a590c6c
davidmato <davidmato2005@gmail.com>
2025-04-02 12:19:59 +0200

    consulta para buscar al jugador por equipo

--- src/main/java/com/example/zonadegolbackend/repository/JugadorRepository.java
+++ src/main/java/com/example/zonadegolbackend/repository/JugadorRepository.java
@@ -1,12 +1,17 @@
 package com.example.zonadegolbackend.repository;
 
+import com.example.zonadegolbackend.entity.Equipo;
 import com.example.zonadegolbackend.entity.Jugador;
 import com.example.zonadegolbackend.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface JugadorRepository extends JpaRepository<Jugador,Integer> {
 
     Jugador findByUsuario(Usuario usuario);
+
+    List<Jugador> findByEquipo(Equipo equipo);
 }
