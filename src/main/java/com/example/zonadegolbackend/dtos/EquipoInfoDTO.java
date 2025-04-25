26426f54ed24591d4e0db82c663efdeeee615229
davidmato <davidmato2005@gmail.com>
2025-04-25 13:46:33 +0200

    dto para la informacion del equipo

---
+++ src/main/java/com/example/zonadegolbackend/dtos/EquipoInfoDTO.java
@@ -0,0 +1,22 @@
+package com.example.zonadegolbackend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class EquipoInfoDTO {
+    private Integer id;
+    private String nombre;
+    private String descripcion;
+    private LocalDate fechaFundacion;
+    private String imagen;
+    private String entrenadorNombre;
+    private String entrenadorImagen;
+    private String ligaNombre;
+    private String jugadorId;
+}
