5e70a2e0e1012a0e295f45a7df2dfdbd224ecd18
davidmato <davidmato2005@gmail.com>
2025-05-14 00:09:19 +0200

    dto de temporada

---
+++ src/main/java/com/example/zonadegolbackend/dtos/TemporadaDTO.java
@@ -0,0 +1,14 @@
+package com.example.zonadegolbackend.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TemporadaDTO {
+
+    private Integer id;
+    private String nombre;
+}
