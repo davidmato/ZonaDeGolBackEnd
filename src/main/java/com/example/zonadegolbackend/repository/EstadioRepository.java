404699adbf5cad6f6addbacd571872d179087df2
MarioCastroRamirez <mcrcdr@gmail.com>
2025-05-28 14:35:07 +0200

    creado estadio DTO para su uso en jornadas y añadidos metodos de busqueda al repository

--- src/main/java/com/example/zonadegolbackend/repository/EstadioRepository.java
+++ src/main/java/com/example/zonadegolbackend/repository/EstadioRepository.java
@@ -2,9 +2,16 @@ package com.example.zonadegolbackend.repository;
 
 import com.example.zonadegolbackend.entity.Estadio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface EstadioRepository extends JpaRepository<Estadio, Integer> {
+
+    @Query("SELECT e FROM Estadio e WHERE e.nombre = :nombre")
+    Optional<Estadio> findByNombre(@Param("nombre") String nombre);
 }
 
