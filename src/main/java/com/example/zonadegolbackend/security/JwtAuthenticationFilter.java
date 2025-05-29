5e60fbfdfbd950f4af15d9c49beee8d8a2727a98
davidmato <davidmato2005@gmail.com>
2025-05-29 11:54:36 +0200

    refactorizacion de nombre

--- src/main/java/com/example/zonadegolbackend/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/zonadegolbackend/security/JwtAuthenticationFilter.java
@@ -31,16 +31,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     ) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
-        final String userEmail;
+        final String userName;
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
             return;
         }
         jwt = authHeader.substring(7);
-        userEmail = jwtService.extractUsername(jwt);
-        System.out.println("Extracted userEmail: " + userEmail); // Add logging here
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        userName = jwtService.extractUsername(jwt);
+        System.out.println("Extracted userName: " + userName); // Add logging here
+        if (userName != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userName);
             if (jwtService.isTokenValid(jwt, userDetails)) {
                 UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
                         userDetails,
