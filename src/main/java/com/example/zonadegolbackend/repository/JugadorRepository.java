60b97e6c26b0032ae5cc64805cdb3b16ce4ccff9
vlexx91 <alemartinenghi@gmail.com>
2025-06-05 23:37:25 +0200

    creado metodo para obtener jugadores por entrenador para campo virtual

--- src/main/java/com/example/zonadegolbackend/repository/JugadorRepository.java
+++ src/main/java/com/example/zonadegolbackend/repository/JugadorRepository.java
@@ -21,4 +21,8 @@ public interface JugadorRepository extends JpaRepository<Jugador,Integer> {
 
     List<Jugador> findByEquipoAndExpulsadoTrue(Equipo equipo);
 
+    List<Jugador> findByEquipo_Entrenador_Id(Integer entrenadorId);
+
+
+
 }
