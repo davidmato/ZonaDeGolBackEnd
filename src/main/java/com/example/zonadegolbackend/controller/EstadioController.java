47d0dcabea98abc12a6a4dc2ea2770eb6845c119
MarioCastroRamirez <mcrcdr@gmail.com>
2025-06-10 16:09:15 +0200

    creado servicio y controller para estadio

---
+++ src/main/java/com/example/zonadegolbackend/controller/EstadioController.java
@@ -0,0 +1,27 @@
+package com.example.zonadegolbackend.controller;
+
+import com.example.zonadegolbackend.entity.Estadio;
+import com.example.zonadegolbackend.services.EstadioService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/estadio")
+public class EstadioController {
+
+    private final EstadioService estadioService;
+
+    @PostMapping("/crear")
+    public Estadio nuevoEstadio(@RequestBody Estadio estadio) {return estadioService.crearEstadio(estadio);}
+
+    @PutMapping("/editar/{id}")
+    public Estadio actualizarEstadio(@PathVariable Integer id, @RequestBody Estadio estadioActualizado) {
+        return estadioService.editarEstadio(id, estadioActualizado);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void borrarEstadio(@PathVariable Integer id) {
+        estadioService.eliminarEstadio(id);
+    }
+}
