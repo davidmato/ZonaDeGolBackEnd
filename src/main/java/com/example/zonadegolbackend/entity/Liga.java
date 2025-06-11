7578ae3472eaa71762712c7bd04daa1c571b07a3
davidmato <davidmato2005@gmail.com>
2025-06-11 22:30:53 +0200

    nuevos campos implementados en la BBDD

--- src/main/java/com/example/zonadegolbackend/entity/Liga.java
+++ src/main/java/com/example/zonadegolbackend/entity/Liga.java
@@ -37,4 +37,7 @@ public class Liga {
 
     @Column(name="fecha_fundacion", nullable = false)
     private LocalDate fecha_fundacion;
+
+    @Column(name = "imagen", nullable = true)
+    private String imagen;
 }
