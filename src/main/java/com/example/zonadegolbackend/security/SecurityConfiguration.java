7e75636cff10d54cf921524d421319b9cb94d1cd
davidmato <davidmato2005@gmail.com>
2025-06-17 00:31:24 +0200

    prueba conexion con front desplegado v1

--- src/main/java/com/example/zonadegolbackend/security/SecurityConfiguration.java
+++ src/main/java/com/example/zonadegolbackend/security/SecurityConfiguration.java
@@ -67,7 +67,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://zonadegolfront.onrender.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
